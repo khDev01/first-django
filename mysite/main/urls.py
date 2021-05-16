5d5abe6bdd51026018cc9d7a28625a233f46eb17
khDev01 <hussaink01@outlook.com>
2021-05-16 16:05:31 +0500

    Created form with redirect showing data added to db with post

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 path("<int:id>", views.index, name="index"),
 path("start/", views.index2, name="start"),
 path("", views.home, name="home"),
+path("create/", views.create, name="create"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
@@ -14,3 +14,17 @@ def index2(response):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        # saves if valid fields are filled
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+            return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
